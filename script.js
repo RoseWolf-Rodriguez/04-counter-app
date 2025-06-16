6d45abfc3098b3e4c375d8b0275c2c11e558eaa4
Lucretia Teretto <lteretto@asu.edu>
2025-06-16 03:45:04 +0000

    Refactor counter functionality to improve readability and maintainability

--- script.js
+++ script.js
@@ -2,14 +2,27 @@
 const countDisplay = document.getElementById("count");
 // Get the "Enter" button. We use const because we do not reassign this variable.
 const increaseButton = document.getElementById("increase");
+// Get the Exit button. We use const because we do not reassign this variable.
+const decreaseButton = document.getElementById("decrease");
 
 // We use let for count because its value will change.
 let count = 0;
 
+// Show the initial count
+countDisplay.textContent = "" + count;
+
 // Add a click event listener to the "Enter" button
 increaseButton.addEventListener("click", function() {
   // Increase the count by 1
-  count = count + 1;
+  count ++;
   // Update the display
   countDisplay.textContent = count;
 });
+
+// When the Exit button is clicked, decrease the count, but not below 0
+decreaseButton.onclick = function() {
+  if (count > 0) {
+    count = count - 1;
+    countDisplay.textContent = "" + count;
+  }
+};
